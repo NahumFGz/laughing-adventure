89b00efa83654010a76b58975087d6040cc78e28
NahumFG <ngutierrez2507@gmail.com>
2024-06-13 02:40:41 -0500

    refact models gpu

--- replicas/20_examen/utils/base_models_gpu.py
+++ replicas/20_examen/utils/base_models_gpu.py
@@ -1,14 +1,9 @@
 from catboost import CatBoostClassifier
 from cuml.ensemble import RandomForestClassifier as cuMLRandomForestClassifier
-from cuml.experimental.ensemble import (
-    HistGradientBoostingClassifier as cuMLHistGradientBoostingClassifier,
-)
 from cuml.linear_model import LogisticRegression as cuMLLogisticRegression
 from cuml.naive_bayes import GaussianNB as cuMLGaussianNB
 from cuml.neighbors import KNeighborsClassifier as cuMLKNeighborsClassifier
-from cuml.neural_network import MLPClassifier as cuMLMLPClassifier
 from cuml.svm import SVC as cuMLSVC
-from cuml.tree import DecisionTreeClassifier as cuMLDecisionTreeClassifier
 from lightgbm import LGBMClassifier
 from xgboost import XGBClassifier
 
@@ -21,11 +16,9 @@ class BaseModels:
     - 'logistic_regression': Regresión Logística.
     - 'decision_tree': Árbol de Decisión.
     - 'random_forest': Bosque Aleatorio.
-    - 'hist_gradient_boosting': Gradient Boosting basado en histogramas.
     - 'svm': Support Vector Machine.
     - 'knn': K-Nearest Neighbors.
     - 'naive_bayes': Naive Bayes.
-    - 'mlp': Perceptrón Multicapa (Red Neuronal).
     - 'lgbm': LightGBM Classifier.
     - 'catboost': CatBoost Classifier.
     - 'xgboost': XGBoost Classifier.
@@ -49,20 +42,14 @@ class BaseModels:
         """
         if method == "logistic_regression":
             return cuMLLogisticRegression(random_state=self.random_state)
-        elif method == "decision_tree":
-            return cuMLDecisionTreeClassifier(random_state=self.random_state)
         elif method == "random_forest":
             return cuMLRandomForestClassifier(random_state=self.random_state)
-        elif method == "hist_gradient_boosting":
-            return cuMLHistGradientBoostingClassifier(random_state=self.random_state)
         elif method == "svm":
             return cuMLSVC(probability=True, random_state=self.random_state)
         elif method == "knn":
             return cuMLKNeighborsClassifier()
         elif method == "naive_bayes":
             return cuMLGaussianNB()
-        elif method == "mlp":
-            return cuMLMLPClassifier(random_state=self.random_state)
         elif method == "lgbm":
             return LGBMClassifier(random_state=self.random_state)
         elif method == "catboost":
